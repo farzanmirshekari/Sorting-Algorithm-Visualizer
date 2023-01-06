28a0867ffa6a30e76aad6a8740c923e1bd8aa158
Farzan Mirshekari <fmirshek@uwaterloo.ca>
2023-01-05 19:47:23 -0500

    Added slider to adjust number of bars..

--- SortingVisualizer.java
+++ SortingVisualizer.java
@@ -4,22 +4,26 @@ import java.util.concurrent.TimeUnit;
 
 import javax.swing.*;
 
-public class Main extends JFrame {
+public class SortingVisualizer extends JFrame {
     private static final int WIDTH = 1200;
+    private static final int CANVAS_WIDTH = 1000;
     private static final int HEIGHT = 900;
-    private static final int NUM_BARS = 120;
+    private static final int MIN_BARS = 10;
+    private static final int MAX_BARS = 200;
+    private static final int INITIAL_BARS = 120;
     private static final int MIN_BAR_LENGTH = HEIGHT / 10;
     private static final int MAX_BAR_LENGTH = HEIGHT - 100;
-    private static final int DELAY = 1; 
+    private static final int DELAY = 1;
 
     private int[] array;
     private JPanel panel;
     private JButton start_button;
+    private JSlider slider;
     private boolean is_sorted;
 
-    public Main() {
-        array = new int[NUM_BARS];
-        for (int i = 0; i < NUM_BARS; i++) {
+    public SortingVisualizer() {
+        array = new int[INITIAL_BARS];
+        for (int i = 0; i < INITIAL_BARS; i++) {
             array[i] = MIN_BAR_LENGTH + (int)(Math.random() * (MAX_BAR_LENGTH - MIN_BAR_LENGTH + 1));
         }
 
@@ -27,12 +31,12 @@ public class Main extends JFrame {
             @Override
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                int width = (int)((double)getWidth() / NUM_BARS);
-                for (int i = 0; i < NUM_BARS; i++) {
+                int width = CANVAS_WIDTH / get_number_of_bars();
+                for (int i = 0; i < get_number_of_bars(); i++) {
                     int x = i * width;
                     int y = getHeight() - array[i];
                     int height = array[i];
-                    g.setColor(Color.BLUE);
+                    g.setColor(new Color(72, 142, 199));
                     g.fillRect(x, y, width, height);
                     g.setColor(Color.BLACK);
                     g.drawRect(x, y, width, height);
@@ -50,13 +54,24 @@ public class Main extends JFrame {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        bubbleSort();
+                        bubble_sort();
                     }
                 }).start();
             }
         });
 
+        slider = new JSlider(JSlider.HORIZONTAL, MIN_BARS, MAX_BARS, INITIAL_BARS);
+        slider.addChangeListener(e -> {
+            array = new int[get_number_of_bars()];
+            for (int i = 0; i < get_number_of_bars(); i++) {
+                array[i] = MIN_BAR_LENGTH + (int)(Math.random() * (MAX_BAR_LENGTH - MIN_BAR_LENGTH + 1));
+            }
+            is_sorted = false;
+            panel.repaint();
+        });
+
         setLayout(new BorderLayout());
+        add(slider, BorderLayout.NORTH);
         add(panel, BorderLayout.CENTER);
         add(start_button, BorderLayout.SOUTH);
 
@@ -68,10 +83,14 @@ public class Main extends JFrame {
         setVisible(true);
     }
 
-    private void bubbleSort() {
+    private int get_number_of_bars() {
+        return slider.getValue();
+    }
+
+    private void bubble_sort() {
         while (!is_sorted) {
             is_sorted = true;
-            for (int i = 0; i < NUM_BARS - 1; i++) {
+            for (int i = 0; i < get_number_of_bars() - 1; i++) {
                 if (array[i] > array[i + 1]) {
                     int temp = array[i];
                     array[i] = array[i + 1];
@@ -89,6 +108,6 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        new Main();
+        new SortingVisualizer();
     }
 }
